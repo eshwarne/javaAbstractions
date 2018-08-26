b6163902df18303992774daf012d966e60fc0a07
Lenovo <Lenovo@DESKTOP-D0J41TV>
2018-08-26 21:41:08 +0530

    getPos initiated

--- javaAbst/src/dataStructures/LinkedList.java
+++ javaAbst/src/dataStructures/LinkedList.java
@@ -5,12 +5,26 @@ public class LinkedList {
 	public LinkedList(Element element){
 		this.head=element;
 	}
+	public void getPos(int pos) {
+		
+	}
 	public void insert(int pos,Element ele) {
 		int i=0;
 		Element current=this.head;
 		if (pos==i) {
 			this.head=ele;
 			ele.next=current;
+			return;
+		}
+		else {
+			for(i=0;i<pos;i++){
+				if(i==pos-1) {
+					
+				}
+				else {
+					current=current.next;
+				}
+			}
 		}
 	}
 }
